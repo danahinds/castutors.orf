376a6679d9ddd29b8c009717b26e0a1cd1c13a6f
Shawn Sandy <creative@shawnsandy.design>
2021-03-16 07:16:45 -0400

    Update .env
    - change contact_provider to from_provider
    
    - update files to match .env changes

--- system/_data/env.js
+++ system/_data/env.js
@@ -3,9 +3,8 @@ require('dotenv').config()
 module.exports = {
   hello: process.env.HELLO || 'Hello not set, but hi, anyway 👋',
   node_env: process.env.NODE_ENV || null,
-  site_map_url: process.env.SITE_MAP_URL || 'https//',
+  site_map_url: process.env.SITE_MAP_URL || 'https//site.com',
   contact_email: process.env.CONTACT_EMAIL || 'site@amce.inc',
-  webforms_success: process.env.WEBFORMS_SUCCESS || 'https://web3forms.com/success',
   contact_success: process.CONTACT_SUCCESS || '/thank-you',
-  contact_provider: process.env.CONTACT_PROVIDER || null
+  form_provider: process.env.FORM_PROVIDER || null
 }
