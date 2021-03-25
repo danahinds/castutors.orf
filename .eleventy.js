feda34655f83e125f594f5ffa9a76543508d1048
Shawn Sandy <creative@shawnsandy.design>
2021-03-25 12:43:58 -0400

    Docs: Add/setup base docs

--- .eleventy.js
+++ .eleventy.js
@@ -76,8 +76,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addCollection("developer", (collection) =>
     collection.getFilteredByTags("developers").sort((a, b) => {
-      if (a.data.title > b.data.title) return -1
-      else if (a.data.title < b.data.title) return 1
+      if (a.data.page.fileSlug > b.data.page.fileSlug) return -1
+      else if (a.data.page.fileSlug < b.data.page.fileSlug) return 1
       else return 0
     })
   )
